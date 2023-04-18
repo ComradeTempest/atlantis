30af9d97d19c0fa42c92e738a15178e115754f83
ComradeTempest <98019531+ComradeTempest@users.noreply.github.com>
2023-04-18 15:02:11 +0300

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,8 @@
+resource "yandex_vpc_network" "atlantis-network" {
+  name = "atlantis-network"
+}
+resource "yandex_vpc_subnet" "atlantis-subnet1" {
+  name           = "atlantis-subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.atlantis-network.id
+  v4_cidr_blocks = ["192.168.113.0/24"]
